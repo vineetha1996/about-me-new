4ea62a1f408161f527a77bc66befdeca31560995
Vineetha Yenugula <vineethay96@gmail.com>
2020-03-16 15:24:23 -0500

    Modified logic on app.html page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-
-const mult = (i, j) => { return i*j }
-
-const updateWithAdd = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  
-    const i = parseInt(document.querySelector('#firstInput').value)
-    const j = parseInt(document.querySelector('#secondInput').value)
-    const ans = `Total space required is ${mul(i, j)}.`
-    document.querySelector('#result').innerHTML = ans
+function mult(i, j){
+  if (i > 0 && j > 0){
+    return i*j;
+  } else {
+    return null;
+    ans = "Please enter valid length and breath dimensions";
   }
+  
+}
+
+function updateWithMult(){
+  document.querySelector('#result').innerHTML = ''
+  let ans = '', result = '';
+  const i = parseInt(document.querySelector('#firstInput').value)
+  const j = parseInt(document.querySelector('#secondInput').value)
+  
+  ans = mult(i, j);
+  if( ans > 0) {
+    result = "Total space required is " + ans;
+  } else {
+    result = "Please enter valid length and breath dimensions";
+  }
+  
+  document.querySelector('#result').innerHTML = result;
+} 
 
   document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
-})
+    if (event.target && event.target.id === 'addButton') { 
+      updateWithMult(event) 
+    }
+  })
--- js/maintest.js
+++ js/maintest.js
@@ -1,13 +1,5 @@
 QUnit.module('MAIN MODULE', {})  // group all these tests together
 
-QUnit.test('TEST add', assert => {
-  assert.equal(mult(1, 2), 2, 'Positive integers')
-  assert.equal(mult(8,-2), -16, 'Mixed')
-  assert.equal(mult(-1, -7), 7, 'Negative integers')
-  assert.equal(mult(-1, 10), -10, 'Mixed')
-  assert.equal(mult(0,3),0, 'zero')
-})
-
 QUnit.config.autostart = false  // sync = false; start after loading html
 
 window.addEventListener('load', () => {
@@ -31,17 +23,4 @@ window.addEventListener('load', () => {
       console.error('error:', error);
       QUnit.start()
     })
-})
-function mult(x,y){
-    return x*y
-}
-QUnit.test("TEST first number validation", assert => {
-  const input = document.querySelector('#firstInput')
-  const warning = document.querySelector('#firstWarning')
-  input.value = "a";
-  assert.equal(input.value, "a", "Bad value assigned")
-  assert.strictEqual(input.checkValidity(), false, "Correctly fails validation")
-  input.focus()
-  input.blur()
-  assert.strictEqual(warning.innerHTML, 'Invalid input', `Correctly adds warning ${warning}`)
 })
